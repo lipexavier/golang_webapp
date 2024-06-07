7d87d7c02d5fad3da6b961e17751951184ec5186
Filipe Xavier <filipexaviersilva@outlook.com>
2024-06-07 11:39:29 -0300

    Importação de libs externas (go get), criação de db postgres e tabelas, insert de dados no banco, criação de conexão com o postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	p "golang_webapp/domains/models/produtos"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
+
+func dbConnect() *sql.DB { //Função de criaçao de conexão com o postgres
+	connString := "user=user dbname=loja password=password host=localhost sslmode=disable" //Configuração da string de conexão (pode ser feito via env)
+	db, err := sql.Open("postgres", connString)                                            //Abre conexão
+	if err != nil {                                                                        //Valida erros
+		panic(err.Error())
+	}
+	return db
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := dbConnect()                 //Tenta conectar ao db
+	defer db.Close()                  //Fecha conexão
 	http.HandleFunc("/", index)       //Define rota para a função index
 	http.ListenAndServe(":8000", nil) //Disponibiliza escuta na porta 8000
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang_webapp
 
 go 1.22.3
+
+require github.com/lib/pq v1.10.9 // indirect
